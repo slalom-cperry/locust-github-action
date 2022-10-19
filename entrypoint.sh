81b78ff51d6bdb2e59651909f9b658dc64c4c4aa
Chris Perry <chris.perry@slalom.com>
2022-10-19 09:50:14 -0700

    add parameter for report path

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ else
     file="/github/workspace/$INPUT_LOCUSTFILE"
 fi
 
-locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL
+locust -f $file --headless -u $INPUT_USERS -r $INPUT_RATE --run-time $INPUT_RUNTIME -H $INPUT_URL --html $IINPUT_REPORTPATH
